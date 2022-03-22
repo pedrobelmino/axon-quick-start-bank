da952a247d093b73220e8f8159c061ed79fc5a1f
pedrobelmino <pedrobelmino@gmail.com>
2022-03-22 20:03:32 -0300

    adicionando lombok e apoio a logs

--- bank-query/src/main/java/br/com/pedrobelmino/axon/labs/bank/model/BankAccount.java
+++ bank-query/src/main/java/br/com/pedrobelmino/axon/labs/bank/model/BankAccount.java
@@ -1,9 +1,6 @@
 package br.com.pedrobelmino.axon.labs.bank.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -14,6 +11,7 @@ import java.math.BigDecimal;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class BankAccount {
 
     @Id
